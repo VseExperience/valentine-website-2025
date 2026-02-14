dfaaa8da89dea5f0178fd87c555cba7dc34758cb
VseExperience <59097012+VseExperience@users.noreply.github.com>
2026-02-14 05:56:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Мила",
+    valentineName: "Милая Мила",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
